42917c63c06611da4e88599b2283da4d2359fbba
Jms147 <steinhauserjames3@gmail.com>
2023-06-01 17:43:36 -0700

    1st draft of url routes

--- controllers/api/urlRoutes.js
+++ controllers/api/urlRoutes.js
@@ -1,2 +1,27 @@
 const router = require("express").Router();
-const { User } = require("../../models");
+const { Url } = require("../../models");
+
+const withAuth = require("../../utils/auth");
+const url = 'https://url-shortener-service.p.rapidapi.com/shorten';
+const options = {
+	method: 'POST',
+	headers: {
+		'content-type': 'application/x-www-form-urlencoded',
+		'X-RapidAPI-Key': '78153fa523msh56908328bb9740dp167b5ejsn8246729d6f2a',
+		'X-RapidAPI-Host': 'url-shortener-service.p.rapidapi.com'
+	},
+	body: new URLSearchParams({
+		url: 'https://google.com/'
+	})
+};
+router.get('/', async (req,res) =>{
+try {
+	const response = await fetch(url, options);
+	const result = await response.text();
+	console.log(result);
+} catch (error) {
+	console.error(error);
+}
+});
+
+module.exports = router;
