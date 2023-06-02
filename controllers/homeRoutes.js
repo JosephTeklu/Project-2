deadcc1cd2a997f5f023c70b76fe338c9f6fa822
Jms147 <steinhauserjames3@gmail.com>
2023-06-01 19:31:11 -0700

    could not figure out how to add post and delete to urlroutes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -3,4 +3,8 @@ const { User, Url } = require("../models");
 const withAuth = require("../utils/auth");
 
 
+router.get('/', async (req,res) => {
+    res.render('homepage', {{User}});
+});
+
 module.exports = router;
