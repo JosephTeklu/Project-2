779721780240931f5ca4fa602786c397325be47b
evviecurran <125322606+evviecurran@users.noreply.github.com>
2023-06-01 19:08:07 -0600

    auth.js- fixed an error

--- utils/auth.js
+++ utils/auth.js
@@ -1,11 +1,13 @@
-const withAuth = (req, res, next) ==> {
-
+const withAuth = (req, res, next) => {
     //If user isn't logged in, redirect them to the login route
         if (!req.session.logged_in) {
             res.redirect('/login');
         } else {
             next();
         }
-    };
+ };
     
     module.exports = withAuth;
+
+
+      
