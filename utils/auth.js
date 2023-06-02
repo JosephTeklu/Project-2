8f05997aea2b15201074e322da311b457e744f12
evviecurran <125322606+evviecurran@users.noreply.github.com>
2023-06-01 19:18:35 -0600

    auth.js, ensures users login

--- utils/auth.js
+++ utils/auth.js
@@ -9,16 +9,3 @@ const withAuth = (req, res, next) => {
  };
     
     module.exports = withAuth;
-
-
-      
-
-const withAuth = (req,res,next) => {
-if (!req.session.logged_in) {
-    res.redirect('/login');
-} else {
-    next();
-}
-};
-
-module.exports = withAuth;
