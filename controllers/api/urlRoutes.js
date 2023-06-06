14d2764fa976c91f5e4cfb707e352125564e3653
Jms147 <steinhauserjames3@gmail.com>
2023-06-05 17:41:45 -0700

    almost done with url routes and added url.js in public

--- controllers/api/urlRoutes.js
+++ controllers/api/urlRoutes.js
@@ -3,6 +3,8 @@ const { Url } = require("../../models");
 
 const withAuth = require("../../utils/auth");
 const url = 'https://url-shortener-service.p.rapidapi.com/shorten';
+
+
 const options = {
 	method: 'POST',
 	headers: {
@@ -11,44 +13,72 @@ const options = {
 		'X-RapidAPI-Host': process.env.APIHOST
 	},
 	body: new URLSearchParams({
-		url: ''
+		url: "https://google.com"
 	})
 };
 
 //add new url
 router.get('/', async (req,res) =>{
 try {
+	userUrl = "https://facebook.com"
+	const options = {
+		method: 'POST',
+		headers: {
+			'content-type': 'application/x-www-form-urlencoded',
+			'X-RapidAPI-Key': process.env.APIKEY,
+			'X-RapidAPI-Host': process.env.APIHOST
+		},
+		body: new URLSearchParams({
+			url: userUrl
+		})
+	};
 	const newUrl = await fetch(url, options);
 	const result = await newUrl.text(req.body);
 	//save url
 	req.session.save(() => {
 		req.session.url_id = newUrl.id;
-		res.status(200).json(Url);
+		res.status(200).json(result);
+		console.log(result);
 	});
-	console.log(result);
 } catch (error) {
-	console.error(error);
+	res.status(500).json(error);
 }
+
 });
 
 router.post('/', async (req,res) => {
 	try {
-		const newUrl = await Url.create.fetch(url, options);
+		userUrl = "https://bing.com"
+		const options = {
+			method: 'POST',
+			headers: {
+				'content-type': 'application/x-www-form-urlencoded',
+				'X-RapidAPI-Key': process.env.APIKEY,
+				'X-RapidAPI-Host': process.env.APIHOST
+			},
+			body: new URLSearchParams({
+				url: req.body
+			})
+		};
+		const newUrl = await fetch(url, options);
+		const result = await newUrl.text(req.body)
 		
 		req.session.save (() => {
 			req.session.url_id = newUrl.id;
-			res.status(200).json(newUrl);
+			res.status(200).json(result);
+			console.log(result)
 		});
 	} catch (error) {
 		res.status(500).json(error);
 	}
+	
 });
 
 router.delete('/', async (req,res) => {
 	try{
 		await Url.destroy(req.body);
 		req.session.save(() => {
-			res.status(200).json(e);
+			res.status(200).json();
 		})
 	} catch (error) {
 		res.status(500).json(error)
